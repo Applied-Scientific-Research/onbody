8763a4d2bf8c3967b2d2e676b6649370c64fd860
Mark Stock <markjstock@gmail.com>
2020-04-22 18:11:17 -0400

    Renamed the libraries and interface sources, added a 3d vort with grads library

---
+++ src/interface3dvortgrads.cpp
@@ -0,0 +1,349 @@
+/*
+ * interface3dvortgrads.cpp - interface code for Barnes-Hut treecode
+ *
+ * Copyright (c) 2017-20, Mark J Stock <markjstock@gmail.com>
+ */
+
+#include <cstdlib>
+#include <cstdint>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+#include <iostream>
+#include <chrono>
+
+
+//
+// The inner, scalar kernel
+//
+template <class S, class A>
+static inline void nbody_kernel(const S sx, const S sy, const S sz,
+                                const S ssx, const S ssy, const S ssz, const S sr,
+                                const S tx, const S ty, const S tz,
+                                A& __restrict__ tu, A& __restrict__ tv, A& __restrict__ tw,
+                                A& __restrict__ tux, A& __restrict__ tvx, A& __restrict__ twx,
+                                A& __restrict__ tuy, A& __restrict__ tvy, A& __restrict__ twy,
+                                A& __restrict__ tuz, A& __restrict__ tvz, A& __restrict__ twz) {
+    // 63 flops
+    const S dx = tx - sx;
+    const S dy = ty - sy;
+    const S dz = tz - sz;
+    const S r2 = dx*dx + dy*dy + dz*dz + sr*sr;
+    const S r3 = (S)1.0 / (r2*std::sqrt(r2));
+    const S bbb = S(-3.0) * r3 / r2;
+    S dxxw = dz*ssy - dy*ssz;
+    S dyxw = dx*ssz - dz*ssx;
+    S dzxw = dy*ssx - dx*ssy;
+    tu += r3 * dxxw;
+    tv += r3 * dyxw;
+    tw += r3 * dzxw;
+    dxxw *= bbb;
+    dyxw *= bbb;
+    dzxw *= bbb;
+    tux += dx*dxxw;
+    tvx += dx*dyxw + ssz*r3;
+    twx += dx*dzxw - ssy*r3;
+    tuy += dy*dxxw - ssz*r3;
+    tvy += dy*dyxw;
+    twy += dy*dzxw + ssx*r3;
+    tuz += dz*dxxw + ssy*r3;
+    tvz += dz*dyxw - ssx*r3;
+    twz += dz*dzxw;
+}
+
+static inline int nbody_kernel_flops() { return 63; }
+
+template <class S, class A, int PD, int SD, int OD> class Parts;
+
+template <class S, class A, int PD, int SD, int OD>
+void ppinter(const Parts<S,A,PD,SD,OD>& __restrict__ srcs,  const size_t jstart, const size_t jend,
+                   Parts<S,A,PD,SD,OD>& __restrict__ targs, const size_t i) {
+    //printf("    compute srcs %ld-%ld on targ %ld\n", jstart, jend, i);
+    for (size_t j=jstart; j<jend; ++j) {
+        nbody_kernel(srcs.x[0][j],  srcs.x[1][j],  srcs.x[2][j],
+                     srcs.s[0][j],  srcs.s[1][j],  srcs.s[2][j],
+                     srcs.r[j],
+                     targs.x[0][i], targs.x[1][i], targs.x[2][i],
+                     targs.u[0][i], targs.u[1][i], targs.u[2][i],
+                     targs.u[3][i], targs.u[4][i], targs.u[5][i],
+                     targs.u[6][i], targs.u[7][i], targs.u[8][i],
+                     targs.u[9][i], targs.u[10][i],targs.u[11][i]);
+    }
+}
+
+template <class S, class A, int PD, int SD, int OD>
+void ppinter(const Parts<S,A,PD,SD,OD>& __restrict__ srcs,  const size_t jstart, const size_t jend,
+                   Parts<S,A,PD,SD,OD>& __restrict__ targs, const size_t istart, const size_t iend) {
+    //printf("    compute srcs %ld-%ld on targs %ld-%ld\n", jstart, jend, istart, iend);
+    for (size_t i=istart; i<iend; ++i) {
+        for (size_t j=jstart; j<jend; ++j) {
+            nbody_kernel(srcs.x[0][j],  srcs.x[1][j],  srcs.x[2][j],
+                     srcs.s[0][j],  srcs.s[1][j],  srcs.s[2][j],
+                     srcs.r[j],
+                     targs.x[0][i], targs.x[1][i], targs.x[2][i],
+                     targs.u[0][i], targs.u[1][i], targs.u[2][i],
+                     targs.u[3][i], targs.u[4][i], targs.u[5][i],
+                     targs.u[6][i], targs.u[7][i], targs.u[8][i],
+                     targs.u[9][i], targs.u[10][i],targs.u[11][i]);
+    }
+    }
+}
+
+template <class S, int PD, int SD> class Tree;
+
+template <class S, class A, int PD, int SD, int OD>
+void tpinter(const Tree<S,PD,SD>& __restrict__ stree, const size_t j,
+                   Parts<S,A,PD,SD,OD>& __restrict__ targs, const size_t i) {
+    //printf("    compute srcs %ld-%ld on targ %ld\n", jstart, jend, i);
+    nbody_kernel(stree.x[0][j], stree.x[1][j], stree.x[2][j],
+                     stree.s[0][j], stree.s[1][j], stree.s[2][j],
+                     stree.pr[j],
+                     targs.x[0][i], targs.x[1][i], targs.x[2][i],
+                     targs.u[0][i], targs.u[1][i], targs.u[2][i],
+                     targs.u[3][i], targs.u[4][i], targs.u[5][i],
+                     targs.u[6][i], targs.u[7][i], targs.u[8][i],
+                     targs.u[9][i], targs.u[10][i],targs.u[11][i]);
+}
+
+//
+// Now we can include the tree-building and recursion code
+//
+#include "barneshut.h"
+//
+//
+
+
+//
+// call this function from an external program
+//
+extern "C" float external_vel_solver_f_ (const int* nsrc,
+                                         const float* sx, const float* sy, const float* sz,
+                                         const float* ssx, const float* ssy, const float* ssz,
+                                         const float* sr,
+                                         const int* ntarg,
+                                         const float* tx, const float* ty, const float* tz,
+                                         float* tu,  float* tv,  float* tw,
+                                         float* tux, float* tvx, float* twx,
+                                         float* tuy, float* tvy, float* twy,
+                                         float* tuz, float* tvz, float* twz) {
+    float flops = 0.0;
+    const bool silent = false;
+    const bool createTargTree = true;
+    const bool blockwise = true;
+
+    if (!silent) printf("Allocate and initialize\n");
+    auto start = std::chrono::system_clock::now();
+
+    // allocate space for sources and targets
+    Parts<float,double,3,3,12> srcs(*nsrc);
+    // initialize particle data
+    for (int i=0; i<*nsrc; ++i) srcs.x[0][i] = sx[i];
+    for (int i=0; i<*nsrc; ++i) srcs.x[1][i] = sy[i];
+    for (int i=0; i<*nsrc; ++i) srcs.x[2][i] = sz[i];
+    for (int i=0; i<*nsrc; ++i) srcs.s[0][i] = ssx[i];
+    for (int i=0; i<*nsrc; ++i) srcs.s[1][i] = ssy[i];
+    for (int i=0; i<*nsrc; ++i) srcs.s[2][i] = ssz[i];
+    for (int i=0; i<*nsrc; ++i) srcs.r[i] = sr[i];
+
+    Parts<float,double,3,3,12> targs(*ntarg);
+    // initialize particle data
+    for (int i=0; i<*ntarg; ++i) targs.x[0][i] = tx[i];
+    for (int i=0; i<*ntarg; ++i) targs.x[1][i] = ty[i];
+    for (int i=0; i<*ntarg; ++i) targs.x[2][i] = tz[i];
+    for (auto& m : targs.s[0]) { m = 1.0f; }
+    for (auto& u : targs.u[0]) { u = 0.0f; }
+    for (auto& u : targs.u[1]) { u = 0.0f; }
+    for (auto& u : targs.u[2]) { u = 0.0f; }
+    for (auto& u : targs.u[3]) { u = 0.0f; }
+    for (auto& u : targs.u[4]) { u = 0.0f; }
+    for (auto& u : targs.u[5]) { u = 0.0f; }
+    for (auto& u : targs.u[6]) { u = 0.0f; }
+    for (auto& u : targs.u[7]) { u = 0.0f; }
+    for (auto& u : targs.u[8]) { u = 0.0f; }
+    for (auto& u : targs.u[9]) { u = 0.0f; }
+    for (auto& u : targs.u[10]) { u = 0.0f; }
+    for (auto& u : targs.u[11]) { u = 0.0f; }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    if (!silent) printf("  init parts time:\t\t[%.4f] seconds\n", elapsed_seconds.count());
+
+
+    // initialize and generate tree
+    if (!silent) printf("\nBuilding the source tree\n");
+    if (!silent) printf("  with %d particles and block size of %ld\n", *nsrc, blockSize);
+    start = std::chrono::system_clock::now();
+    Tree<float,3,3> stree(0);
+    // split this node and recurse
+    (void) makeTree(srcs, stree);
+    end = std::chrono::system_clock::now(); elapsed_seconds = end-start;
+    if (!silent) printf("  build tree time:\t\t[%.4f] seconds\n", elapsed_seconds.count());
+
+    // find equivalent particles
+    if (!silent) printf("\nCalculating equivalent particles\n");
+    start = std::chrono::system_clock::now();
+    Parts<float,double,3,3,12> eqsrcs((stree.numnodes/2) * blockSize);
+    if (!silent) printf("  need %ld particles\n", eqsrcs.n);
+    end = std::chrono::system_clock::now(); elapsed_seconds = end-start;
+    if (!silent) printf("  allocate eqsrcs structures:\t[%.4f] seconds\n", elapsed_seconds.count());
+
+    // reorder tree until all parts are adjacent in space-filling curve
+    start = std::chrono::system_clock::now();
+    #pragma omp parallel
+    #pragma omp single
+    (void) refineTree(srcs, stree, 1);
+    #pragma omp taskwait
+    end = std::chrono::system_clock::now(); elapsed_seconds = end-start;
+    if (!silent) printf("  refine within leaf nodes:\t[%.4f] seconds\n", elapsed_seconds.count());
+
+    // then, march through arrays merging pairs as you go up
+    start = std::chrono::system_clock::now();
+    (void) calcEquivalents(srcs, eqsrcs, stree, 1);
+    end = std::chrono::system_clock::now(); elapsed_seconds = end-start;
+    if (!silent) printf("  create equivalent parts:\t[%.4f] seconds\n", elapsed_seconds.count());
+
+
+    Tree<float,3,3> ttree(0);
+    if (createTargTree or blockwise) {
+        if (!silent) printf("\nBuilding the target tree\n");
+        if (!silent) printf("  with %d particles and block size of %ld\n", *ntarg, blockSize);
+
+        start = std::chrono::system_clock::now();
+        // split this node and recurse
+        (void) makeTree(targs, ttree);
+        end = std::chrono::system_clock::now(); elapsed_seconds = end-start;
+        if (!silent) printf("  build tree time:\t\t[%.4f] seconds\n", elapsed_seconds.count());
+    }
+
+
+    //
+    // Run a better O(NlogN) treecode - boxes use equivalent particles
+    //
+    if (!silent) printf("\nRun the treecode O(NlogN) with equivalent particles\n");
+    start = std::chrono::system_clock::now();
+
+    if (blockwise) {
+        flops += nbody_treecode3(srcs, eqsrcs, stree, targs, ttree, 2.5f);
+    } else {
+        flops += nbody_treecode2(srcs, eqsrcs, stree, targs, 2.5f);
+    }
+
+    end = std::chrono::system_clock::now(); elapsed_seconds = end-start;
+    double dt = elapsed_seconds.count();
+    if (!silent) printf("  treecode summations:\t\t[%.4f] seconds\n\n", dt);
+
+
+    // pull results from the object
+    if (createTargTree) {
+        // need to rearrange the results back in original order
+        for (int i=0; i<*ntarg; ++i) tu[targs.gidx[i]] += targs.u[0][i];
+        for (int i=0; i<*ntarg; ++i) tv[targs.gidx[i]] += targs.u[1][i];
+        for (int i=0; i<*ntarg; ++i) tw[targs.gidx[i]] += targs.u[2][i];
+        for (int i=0; i<*ntarg; ++i) tux[targs.gidx[i]] += targs.u[3][i];
+        for (int i=0; i<*ntarg; ++i) tvx[targs.gidx[i]] += targs.u[4][i];
+        for (int i=0; i<*ntarg; ++i) twx[targs.gidx[i]] += targs.u[5][i];
+        for (int i=0; i<*ntarg; ++i) tuy[targs.gidx[i]] += targs.u[6][i];
+        for (int i=0; i<*ntarg; ++i) tvy[targs.gidx[i]] += targs.u[7][i];
+        for (int i=0; i<*ntarg; ++i) twy[targs.gidx[i]] += targs.u[8][i];
+        for (int i=0; i<*ntarg; ++i) tuz[targs.gidx[i]] += targs.u[9][i];
+        for (int i=0; i<*ntarg; ++i) tvz[targs.gidx[i]] += targs.u[10][i];
+        for (int i=0; i<*ntarg; ++i) twz[targs.gidx[i]] += targs.u[11][i];
+        //for (int i=0; i<*ntarg; ++i) printf("  %d %ld  %g %g\n", i, targs.gidx[i], tu[i], tv[i]);
+    } else {
+        // pull them out directly
+        for (int i=0; i<*ntarg; ++i) tu[i] += targs.u[0][i];
+        for (int i=0; i<*ntarg; ++i) tv[i] += targs.u[1][i];
+        for (int i=0; i<*ntarg; ++i) tw[i] += targs.u[2][i];
+        for (int i=0; i<*ntarg; ++i) tux[i] += targs.u[3][i];
+        for (int i=0; i<*ntarg; ++i) tvx[i] += targs.u[4][i];
+        for (int i=0; i<*ntarg; ++i) twx[i] += targs.u[5][i];
+        for (int i=0; i<*ntarg; ++i) tuy[i] += targs.u[6][i];
+        for (int i=0; i<*ntarg; ++i) tvy[i] += targs.u[7][i];
+        for (int i=0; i<*ntarg; ++i) twy[i] += targs.u[8][i];
+        for (int i=0; i<*ntarg; ++i) tuz[i] += targs.u[9][i];
+        for (int i=0; i<*ntarg; ++i) tvz[i] += targs.u[10][i];
+        for (int i=0; i<*ntarg; ++i) twz[i] += targs.u[11][i];
+    }
+
+    return flops;
+}
+
+
+//
+// same, but direct solver
+//
+extern "C" float external_vel_direct_f_ (const int* nsrc,
+                                         const float* sx, const float* sy, const float* sz,
+                                         const float* ssx, const float* ssy, const float* ssz,
+                                         const float* sr,
+                                         const int* ntarg,
+                                         const float* tx, const float* ty, const float* tz,
+                                         float* tu,  float* tv,  float* tw,
+                                         float* tux, float* tvx, float* twx,
+                                         float* tuy, float* tvy, float* twy,
+                                         float* tuz, float* tvz, float* twz) {
+    float flops = 0.0;
+    bool silent = true;
+
+    if (!silent) printf("Allocate and initialize\n");
+    auto start = std::chrono::system_clock::now();
+
+    // allocate space for sources and targets
+    Parts<float,double,3,3,12> srcs(*nsrc);
+    // initialize particle data
+    for (int i=0; i<*nsrc; ++i) srcs.x[0][i] = sx[i];
+    for (int i=0; i<*nsrc; ++i) srcs.x[1][i] = sy[i];
+    for (int i=0; i<*nsrc; ++i) srcs.x[2][i] = sz[i];
+    for (int i=0; i<*nsrc; ++i) srcs.s[0][i] = ssx[i];
+    for (int i=0; i<*nsrc; ++i) srcs.s[1][i] = ssy[i];
+    for (int i=0; i<*nsrc; ++i) srcs.s[2][i] = ssz[i];
+    for (int i=0; i<*nsrc; ++i) srcs.r[i] = sr[i];
+
+    Parts<float,double,3,3,12> targs(*ntarg);
+    // initialize particle data
+    for (int i=0; i<*ntarg; ++i) targs.x[0][i] = tx[i];
+    for (int i=0; i<*ntarg; ++i) targs.x[1][i] = ty[i];
+    for (int i=0; i<*ntarg; ++i) targs.x[2][i] = tz[i];
+    for (auto& u : targs.u[0]) { u = 0.0f; }
+    for (auto& u : targs.u[1]) { u = 0.0f; }
+    for (auto& u : targs.u[2]) { u = 0.0f; }
+    for (auto& u : targs.u[3]) { u = 0.0f; }
+    for (auto& u : targs.u[4]) { u = 0.0f; }
+    for (auto& u : targs.u[5]) { u = 0.0f; }
+    for (auto& u : targs.u[6]) { u = 0.0f; }
+    for (auto& u : targs.u[7]) { u = 0.0f; }
+    for (auto& u : targs.u[8]) { u = 0.0f; }
+    for (auto& u : targs.u[9]) { u = 0.0f; }
+    for (auto& u : targs.u[10]) { u = 0.0f; }
+    for (auto& u : targs.u[11]) { u = 0.0f; }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    if (!silent) printf("  init parts time:\t\t[%.4f] seconds\n", elapsed_seconds.count());
+
+    //
+    // Run a naive O(N^2) summation
+    //
+    if (!silent) printf("\nRun the direct O(N^2) summation\n");
+    start = std::chrono::system_clock::now();
+    flops += nbody_naive(srcs, targs, 1);
+    end = std::chrono::system_clock::now(); elapsed_seconds = end-start;
+    double dt = elapsed_seconds.count();
+    if (!silent) printf("  direct summations:\t\t[%.4f] seconds\n\n", dt);
+
+    // save the results out
+    for (int i=0; i<*ntarg; ++i) tu[i] += targs.u[0][i];
+    for (int i=0; i<*ntarg; ++i) tv[i] += targs.u[1][i];
+    for (int i=0; i<*ntarg; ++i) tw[i] += targs.u[2][i];
+    for (int i=0; i<*ntarg; ++i) tux[i] += targs.u[3][i];
+    for (int i=0; i<*ntarg; ++i) tvx[i] += targs.u[4][i];
+    for (int i=0; i<*ntarg; ++i) twx[i] += targs.u[5][i];
+    for (int i=0; i<*ntarg; ++i) tuy[i] += targs.u[6][i];
+    for (int i=0; i<*ntarg; ++i) tvy[i] += targs.u[7][i];
+    for (int i=0; i<*ntarg; ++i) twy[i] += targs.u[8][i];
+    for (int i=0; i<*ntarg; ++i) tuz[i] += targs.u[9][i];
+    for (int i=0; i<*ntarg; ++i) tvz[i] += targs.u[10][i];
+
+    return flops;
+}
---
+++ src/main3dvortgrads.cpp
@@ -0,0 +1,209 @@
+/*
+ * main3dvortgrads.cpp - driver for Barnes-Hut treecode
+ *
+ * Copyright (c) 2017-20, Mark J Stock <markjstock@gmail.com>
+ */
+
+#include <cstdlib>
+#include <cstdint>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+#include <vector>
+#include <iostream>
+#include <chrono>
+
+// the fast solver
+extern "C" float external_vel_solver_f_ (const int*,
+                                         const float*, const float*, const float*,
+                                         const float*, const float*, const float*,
+                                         const float*,
+                                         const int*,
+                                         const float*, const float*, const float*,
+                                         float*,  float*,  float*,
+                                         float*, float*, float*,
+                                         float*, float*, float*,
+                                         float*, float*, float*);
+
+// the direct solver
+extern "C" float external_vel_direct_f_ (const int*,
+                                         const float*, const float*, const float*,
+                                         const float*, const float*, const float*,
+                                         const float*,
+                                         const int*,
+                                         const float*, const float*, const float*,
+                                         float*,  float*,  float*,
+                                         float*, float*, float*,
+                                         float*, float*, float*,
+                                         float*, float*, float*);
+
+const char* progname = "main3dvortgrads";
+
+//
+// basic usage
+//
+static void usage() {
+    fprintf(stderr, "Usage: %s [-n=<nparticles>]\n", progname);
+    exit(1);
+}
+
+//
+// main routine - run the program
+//
+int main(int argc, char *argv[]) {
+
+    size_t numSrcs = 10000;
+    size_t numTargs = 10000;
+    bool compareToDirect = true;
+
+    if (argc > 1) {
+        if (strncmp(argv[1], "-n=", 3) == 0) {
+            size_t num = atof(argv[1] + 3);
+            if (num < 1) usage();
+            numSrcs = num;
+            numTargs = num;
+        }
+    }
+
+    printf("Running %s with %ld sources and %ld targets\n", progname, numSrcs, numTargs);
+    auto start = std::chrono::system_clock::now();
+
+    // set up the problem
+
+    std::vector<float> sx(numSrcs);
+    std::vector<float> sy(numSrcs);
+    std::vector<float> sz(numSrcs);
+    std::vector<float> ssx(numSrcs);
+    std::vector<float> ssy(numSrcs);
+    std::vector<float> ssz(numSrcs);
+    std::vector<float> sr(numSrcs);
+    for (auto& _x : sx) { _x = (float)rand()/(float)RAND_MAX; }
+    for (auto& _y : sy) { _y = (float)rand()/(float)RAND_MAX; }
+    for (auto& _z : sz) { _z = (float)rand()/(float)RAND_MAX; }
+    for (auto& _r : sr) { _r = 1.0f / std::sqrt(numSrcs); }
+    // totally random (unrealistic, but worst-case)
+    //for (auto& _m : ss) { _m = (-1.0f + 2.0f*(float)rand()/(float)RAND_MAX) / std::sqrt((float)numSrcs); }
+    // more realistic
+    //const float factor = 1.0 / std::sqrt((float)numSrcs);
+    const float factor = 1.0 / (float)numSrcs;
+    //for (size_t i=0; i<numSrcs; i++) {
+    //    const float dist = std::sqrt(std::pow(sx[i]-0.5,2)+std::pow(sy[i]-0.5,2));
+    //    ssx[i] = factor * std::cos(30.0*std::sqrt(dist)) / (5.0*dist+1.0);
+     //   ssx[i] *= 0.75 + 0.5*(float)rand()/(float)RAND_MAX;
+     //   ssy[i] = factor * std::cos(10.0*std::sqrt(dist)) / (6.0*dist+1.0);
+     //   ssy[i] *= 0.75 + 0.5*(float)rand()/(float)RAND_MAX;
+     //   ssz[i] = factor * std::cos(50.0*std::sqrt(dist)) / (4.0*dist+1.0);
+     //   ssz[i] *= 0.75 + 0.5*(float)rand()/(float)RAND_MAX;
+    //}
+    for (size_t i=0; i<numSrcs; i++) {
+        ssx[i] = factor * std::cos((0+0.7)*10.0*sx[i]);
+        ssy[i] = factor * std::cos((1+0.7)*10.0*sy[i]);
+        ssz[i] = factor * std::cos((2+0.7)*10.0*sz[i]);
+    }
+
+    std::vector<float> tx(numTargs);
+    std::vector<float> ty(numTargs);
+    std::vector<float> tz(numTargs);
+    std::vector<float> tu(numTargs, 0.0f);
+    std::vector<float> tv(numTargs, 0.0f);
+    std::vector<float> tw(numTargs, 0.0f);
+    std::vector<float> tux(numTargs, 0.0f);
+    std::vector<float> tvx(numTargs, 0.0f);
+    std::vector<float> twx(numTargs, 0.0f);
+    std::vector<float> tuy(numTargs, 0.0f);
+    std::vector<float> tvy(numTargs, 0.0f);
+    std::vector<float> twy(numTargs, 0.0f);
+    std::vector<float> tuz(numTargs, 0.0f);
+    std::vector<float> tvz(numTargs, 0.0f);
+    std::vector<float> twz(numTargs, 0.0f);
+    for (auto& _x : tx) { _x = (float)rand()/(float)RAND_MAX; }
+    for (auto& _y : ty) { _y = (float)rand()/(float)RAND_MAX; }
+    for (auto& _z : tz) { _z = (float)rand()/(float)RAND_MAX; }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    float flops = (float)numSrcs*12.f + (float)numTargs*4.f;
+    float gflops = 1.e-9 * flops / (float)elapsed_seconds.count();
+    printf("    problem setup:\t\t[%.4f] seconds at %.3f GFlop/s\n", (float)elapsed_seconds.count(), gflops);
+
+    // and call the solver
+
+    start = std::chrono::system_clock::now();
+    int ns = numSrcs;
+    int nt = numTargs;
+    flops = external_vel_solver_f_(&ns, sx.data(), sy.data(), sz.data(),
+                                        ssx.data(), ssy.data(), ssz.data(), sr.data(),
+                                   &nt, tx.data(), ty.data(), tz.data(),
+                                        tu.data(), tv.data(), tw.data(),
+                                        tux.data(), tvx.data(), twx.data(),
+                                        tuy.data(), tvy.data(), twy.data(),
+                                        tuz.data(), tvz.data(), twz.data());
+
+    end = std::chrono::system_clock::now();
+    elapsed_seconds = end-start;
+    gflops = 1.e-9 * flops / (float)elapsed_seconds.count();
+    printf("    external_vel_solver_f_:\t[%.4f] seconds at %.3f GFlop/s\n", (float)elapsed_seconds.count(), gflops);
+
+    // compare results to a direct solve
+
+    if (compareToDirect) {
+
+        // run a subset direct solve
+        size_t ntskip = std::max(1, (int)((float)numSrcs*(float)numTargs/1.e+10));
+        int ntn = numTargs / ntskip;
+        std::vector<float> txn(ntn);
+        std::vector<float> tyn(ntn);
+        std::vector<float> tzn(ntn);
+        std::vector<float> tun(ntn, 0.0f);
+        std::vector<float> tvn(ntn, 0.0f);
+        std::vector<float> twn(ntn, 0.0f);
+        std::vector<float> tuxn(ntn, 0.0f);
+        std::vector<float> tvxn(ntn, 0.0f);
+        std::vector<float> twxn(ntn, 0.0f);
+        std::vector<float> tuyn(ntn, 0.0f);
+        std::vector<float> tvyn(ntn, 0.0f);
+        std::vector<float> twyn(ntn, 0.0f);
+        std::vector<float> tuzn(ntn, 0.0f);
+        std::vector<float> tvzn(ntn, 0.0f);
+        std::vector<float> twzn(ntn, 0.0f);
+        for (size_t i=0; i<(size_t)ntn; ++i) {
+            const size_t ifast = i * ntskip;
+            txn[i] = tx[ifast];
+            tyn[i] = ty[ifast];
+            tzn[i] = tz[ifast];
+        }
+        start = std::chrono::system_clock::now();
+        flops = external_vel_direct_f_(&ns, sx.data(), sy.data(), sz.data(),
+                                            ssx.data(), ssy.data(), ssz.data(), sr.data(),
+                                       &ntn, txn.data(), tyn.data(), tzn.data(),
+                                             tun.data(), tvn.data(), twn.data(),
+                                             tuxn.data(), tvxn.data(), twxn.data(),
+                                             tuyn.data(), tvyn.data(), twyn.data(),
+                                             tuzn.data(), tvzn.data(), twzn.data());
+        end = std::chrono::system_clock::now();
+        elapsed_seconds = end-start;
+        gflops = 1.e-9 * flops / (float)elapsed_seconds.count();
+        printf("    external_vel_direct_f_:\t[%.4f] seconds at %.3f GFlop/s\n", (float)ntskip*(float)elapsed_seconds.count(), gflops);
+
+        // compute the error
+        float errsum = 0.0;
+        float errcnt = 0.0;
+        float maxerr = 0.0;
+        for (size_t i=0; i<(size_t)ntn; ++i) {
+            const size_t ifast = i * ntskip;
+            float thiserr = tu[ifast]-tun[i];
+            //if (i<4) printf("      %ld %ld  %g %g   %g %g\n", i, ifast, tx[ifast], txn[i], tu[ifast], tun[i]);
+            //printf("      %ld %ld  %g %g   %g %g\n", i, ifast, tx[ifast], txn[i], tu[ifast], tun[i]);
+            errsum += thiserr*thiserr;
+            //if (thiserr*thiserr > maxerr) printf("      %ld %ld  %g %g   %g %g  new max\n", i, ifast, tx[ifast], txn[i], tu[ifast], tun[i]);
+            if (thiserr*thiserr > maxerr) maxerr = thiserr*thiserr;
+            errcnt += tun[i]*tun[i];
+        }
+        printf("    rms error in fast solver:\t%g\n", std::sqrt(errsum/errcnt));
+        printf("    max error in fast solver:\t%g\n", std::sqrt(maxerr/(errcnt/(float)ntn)));
+    }
+
+    return 0;
+}
