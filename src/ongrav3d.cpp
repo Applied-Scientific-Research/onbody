63058df61c99fa3685dceb38b547c7f6916c1bef
Mark Stock <markjstock@gmail.com>
2022-02-17 22:44:41 -0500

    More cleanup to sync codes

--- src/ongrav3d.cpp
+++ src/ongrav3d.cpp
@@ -7,8 +7,8 @@
 #define STORE float
 #define ACCUM float
 
-#define USE_RM_KERNEL
-//#define USE_EXPONENTIAL_KERNEL
+#include "CoreFunc3d.h"
+#include "LeastSquares.h"
 
 #ifdef USE_VC
 #include <Vc/Vc>
@@ -33,6 +33,7 @@ template <class S> using Vector = std::vector<S, Vc::Allocator<S>>;
 #else
 template <class S> using Vector = std::vector<S>;
 #endif
+
 
 //
 // The inner, scalar kernel
@@ -69,13 +70,16 @@ void ppinter(const Parts<S,A,PD,SD,OD>& __restrict__ srcs,  const size_t jstart,
     //Vc::simdize<Vector<S>::const_iterator> sxit, syit, szit, smit, srit;
     const size_t nSrcVec = (jend-jstart + Vc::Vector<S>::Size - 1) / Vc::Vector<S>::Size;
 
+    // a simd type for A with the same number of entries as S
+    typedef Vc::SimdArray<A,Vc::Vector<S>::size()> VecA;
+
     // spread this target over a vector
     const Vc::Vector<S> vtx = targs.x[0][i];
     const Vc::Vector<S> vty = targs.x[1][i];
     const Vc::Vector<S> vtz = targs.x[2][i];
-    Vc::Vector<A> vtu0(0.0f);
-    Vc::Vector<A> vtu1(0.0f);
-    Vc::Vector<A> vtu2(0.0f);
+    VecA vtu0(0.0f);
+    VecA vtu1(0.0f);
+    VecA vtu2(0.0f);
     // reference source data as Vc::Vector<A>
     sxit = srcs.x[0].begin() + jstart;
     syit = srcs.x[1].begin() + jstart;
@@ -83,7 +87,7 @@ void ppinter(const Parts<S,A,PD,SD,OD>& __restrict__ srcs,  const size_t jstart,
     smit = srcs.s[0].begin() + jstart;
     srit = srcs.r.begin() + jstart;
     for (size_t j=0; j<nSrcVec; ++j) {
-        nbody_kernel<Vc::Vector<S>,Vc::Vector<A>>(
+        nbody_kernel<Vc::Vector<S>,VecA>(
                      *sxit, *syit, *szit, *srit, *smit,
                      vtx, vty, vtz, vtu0, vtu1, vtu2);
         // advance the source iterators
@@ -116,14 +120,17 @@ void ppinter(const Parts<S,A,PD,SD,OD>& __restrict__ srcs,  const size_t jstart,
     Vc::simdize<Vector<STORE>::const_iterator> sxit, syit, szit, smit, srit;
     const size_t nSrcVec = (jend-jstart + Vc::Vector<S>::Size - 1) / Vc::Vector<S>::Size;
 
+    // a simd type for A with the same number of entries as S
+    typedef Vc::SimdArray<A,Vc::Vector<S>::size()> VecA;
+
     for (size_t i=istart; i<iend; ++i) {
         // spread this target over a vector
         const Vc::Vector<S> vtx = targs.x[0][i];
         const Vc::Vector<S> vty = targs.x[1][i];
         const Vc::Vector<S> vtz = targs.x[2][i];
-        Vc::Vector<A> vtu0(0.0f);
-        Vc::Vector<A> vtu1(0.0f);
-        Vc::Vector<A> vtu2(0.0f);
+        VecA vtu0(0.0f);
+        VecA vtu1(0.0f);
+        VecA vtu2(0.0f);
         // convert source data to Vc::Vector<S>
         sxit = srcs.x[0].begin() + jstart;
         syit = srcs.x[1].begin() + jstart;
@@ -131,7 +138,7 @@ void ppinter(const Parts<S,A,PD,SD,OD>& __restrict__ srcs,  const size_t jstart,
         smit = srcs.s[0].begin() + jstart;
         srit = srcs.r.begin() + jstart;
         for (size_t j=0; j<nSrcVec; ++j) {
-            nbody_kernel<Vc::Vector<S>,Vc::Vector<A>>(
+            nbody_kernel<Vc::Vector<S>,VecA>(
                          *sxit, *syit, *szit, *srit, *smit,
                          vtx, vty, vtz, vtu0, vtu1, vtu2);
             // advance the source iterators
@@ -174,102 +181,6 @@ void tpinter(const Tree<S,PD,SD>& __restrict__ stree, const size_t j,
 #include "barneshut.h"
 //
 //
-
-//
-// Approximate a spatial derivative from a number of irregularly-spaced points
-//
-template <class S, class A>
-A least_squares_val(const S xt, const S yt, const S zt,
-                    const Vector<S>& x, const Vector<S>& y,
-                    const Vector<S>& z, const Vector<A>& u,
-                    const size_t istart, const size_t iend) {
-
-    //printf("  target point at %g %g %g\n", xt, yt, zt);
-    S sn = 0.0f;
-    S sx = 0.0f;
-    S sy = 0.0f;
-    S sz = 0.0f;
-    S sx2 = 0.0f;
-    S sy2 = 0.0f;
-    S sz2 = 0.0f;
-    S sv = 0.0f;
-    S sxv = 0.0f;
-    S syv = 0.0f;
-    S szv = 0.0f;
-    S sxy = 0.0f;
-    S sxz = 0.0f;
-    S syz = 0.0f;
-    for (size_t i=istart; i<iend; ++i) {
-        const S dx = x[i] - xt;
-        const S dy = y[i] - yt;
-        const S dz = z[i] - zt;
-        const S dist = std::sqrt(dx*dx+dy*dy+dz*dz);
-        //printf("    point %d at %g %g %g dist %g with value %g\n", i, x[i], y[i], z[i], u[i]);
-        //printf("    point %d at %g %g %g dist %g with value %g\n", i, dx, dy, dz, dist, u[i]);
-        const S weight = 1.f / (0.001f + dist);
-        //const float oods = 1.0f / 
-        //nsum
-        // see https://en.wikipedia.org/wiki/Linear_least_squares_%28mathematics%29
-        // must solve a system of equations for ax + by + cz + d = 0
-        // while minimizing the square error, this is a 4x4 matrix solve
-        // ideally while also weighting the data points by their distance
-
-        // compute sums of moments
-        //const float weight = 1.f;
-        sn += weight;
-        sx += weight*dx;
-        sy += weight*dy;
-        sz += weight*dz;
-        sv += weight*u[i];
-        sxy += weight*dx*dy;
-        sxz += weight*dx*dz;
-        syz += weight*dy*dz;
-        sxv += weight*dx*u[i];
-        syv += weight*dy*u[i];
-        szv += weight*dz*u[i];
-        sx2 += weight*dx*dx;
-        sy2 += weight*dy*dy;
-        sz2 += weight*dz*dz;
-    }
-    // 47 flops per iter
-    //printf("    sums are %g %g %g %g %g ...\n", sx, sy, sz, sv, sxy);
-
-    // now begin to solve the equation
-    const S i1 = sx/sxz - sn/sz;
-    const S i2 = sx2/sxz - sx/sz;
-    const S i3 = sxy/sxz - sy/sz;
-    const S i4 = sxv/sxz - sv/sz;
-    const S j1 = sy/syz - sn/sz;
-    const S j2 = sxy/syz - sx/sz;
-    const S j3 = sy2/syz - sy/sz;
-    const S j4 = syv/syz - sv/sz;
-    const S k1 = sz/sz2 - sn/sz;
-    const S k2 = sxz/sz2 - sx/sz;
-    const S k3 = syz/sz2 - sy/sz;
-    const S k4 = szv/sz2 - sv/sz;
-    const S q1 = i3*j1 - i1*j3;
-    const S q2 = i3*j2 - i2*j3;
-    const S q3 = i3*j4 - i4*j3;
-    const S r1 = i3*k1 - i1*k3;
-    const S r2 = i3*k2 - i2*k3;
-    const S r3 = i3*k4 - i4*k3;
-    // 18*3 = 54 flops
-
-    const A b1 = (r2*q3 - r3*q2) / (r2*q1 - r1*q2);
-    // 7 more
-    //printf("    b1 is %g\n", b1);
-    //const float b2 = r3/r2 - b1*r1/r2;
-    //printf("    b2 is %g\n", b2);
-    //const float b3 = j4/j3 - b1*j1/j3 - b2*j2/j3;
-    //printf("    b3 is %g\n", b3);
-    //const float b4 = sv/sz - b1/sz - b2*sx/sz - b3*sy/sz;
-    //printf("    b4 is %g\n", b4);
-
-    // when 16 contributing points, this is 813 flops
-
-    //if (fabs(u[istart]) > 0.0) exit(0);
-    return b1;
-}
 
 //
 // Data structure for accumulating interaction counts
